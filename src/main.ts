a17ea7a97761f36dc9336ab94ebd4f758cddc8b2
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2024-01-11 21:06:46 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import './assets/main.css'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -7,6 +14,8 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.component("font-awesome-icon", FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
