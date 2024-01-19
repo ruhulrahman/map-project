4d8e8b954fe00947d5a1693e44eb892fbc117415
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2024-01-19 11:56:29 +0600

    updated

--- src/components/ModalT.vue
+++ src/components/ModalT.vue
@@ -26,39 +26,20 @@ defineExpose({
 
         <div class="flex flex-row w-full">
           <div class="flex flex-col w-full gap-3">
-
             <div class="flex flex-col gap-3">
 
               <div class="flex p-2 border-b border-gray-600 bg-black  bg-opacity-[0.7] mx-[-8px] mt-[-8px]">
                 <font-awesome-icon @click="toggleRightSidebar" class="btn-icon" :icon="['fas', 'times']" />
                 <div class="flex-1 pl-2 text-cyan-500">
-                  <h6>My name is Ruhul. How are you are you ok?</h6>
+                  <header>
+                    <slot name="header"></slot>
+                  </header>
                 </div>
               </div>
 
-              <div class="flex-1 bg-black bg-opacity-[0.4] rounded-lg p-2 shadow-cyan-sm shadow-sm shadow-purple-300">
-
-                <div class="mb-2 pb-5">
-                  <label for="success" class="block mb-2 text-sm font-medium text-purple-300">Your
-                    name</label>
-                  <input type="text" id="success"
-                    class=" text-purple-300 placeholder-purple-500 text-sm rounded-lg block w-full p-2.5 shadow-gray-outset focus:shadow-gray-inset"
-                    placeholder="Success input" />
-                </div>
-
-                <div class="mb-2 pb-5 ">
-                  <label for="success" class="block mb-2 text-sm font-medium text-purple-300">Your
-                    name</label>
-                  <input type="text" id="success"
-                    class=" text-purple-300 placeholder-purple-500 text-sm rounded-lg block w-full p-2.5 shadow-gray-outset focus:shadow-gray-inset"
-                    placeholder="Success input" />
-                </div>
-
-              </div>
-
+              <slot></slot>
 
             </div>
-
           </div>
         </div>
       </div>
