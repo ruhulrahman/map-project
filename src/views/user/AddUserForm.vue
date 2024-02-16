7c0b66968a690ef1db06533e5a7d170669f01bce
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2024-02-16 11:07:13 +0600

    updated

--- src/views/user/AddUserForm.vue
+++ src/views/user/AddUserForm.vue
@@ -54,32 +54,15 @@ const initialErrors = {
     // 'email': 'This email is already taken',
 };
 
-
-
-function updateValues() {
-  // set a single field value
-  // setFieldValue('email', 'test@example.com');
-
-  // set multiple fields values
-  setValues({
-    email: 'test@example.com',
-  });
-}
-
-
-// const schema = yup.object({
-//     id: yup.string().default('').label('Id'),
-//     userid: yup.string().default(20).label('User Id'),
-//     spilitter: yup.string().required().label('Splitter'),
-//     usertype: yup.string().required().label('User Type'),
-//     userlatlong: yup.string().required().default('nothing').label('User latlong'),
-//     tjnumber: yup.string().required().label('Tj Number'),
-//     lineby: yup.string().required().min(2).label('Line By'),
-//     loginusername: yup.string().required().min(2).default('Ruhul').label('Username'),
-// });
-
 const schema = yup.object({
-    email: yup.string().min(5).label('Email'),
+    id: yup.string().label('Id'),
+    userid: yup.string().label('User Id'),
+    spilitter: yup.string().required().label('Splitter'),
+    usertype: yup.string().required().label('User Type'),
+    userlatlong: yup.string().required().label('User latlong'),
+    tjnumber: yup.number().required().label('Tj Number'),
+    lineby: yup.string().required().min(2).label('Line By'),
+    loginusername: yup.string().required().min(2).label('Username'),
 });
 
 // const userLatLong = ref('')
@@ -87,30 +70,57 @@ const { errors, setErrors, setValues, setFieldValue, defineField, handleSubmit }
     validationSchema: schema,
 });
 
-const [email, emailAttrs] = defineField('email');
-const [password, passwordAttrs] = defineField('password');
+const [id] = defineField('id');
+const [userid] = defineField('userid');
+const [spilitter] = defineField('spilitter');
+const [usertype] = defineField('usertype');
+const [userlatlong] = defineField('userlatlong');
+const [tjnumber] = defineField('tjnumber');
+const [lineby] = defineField('lineby');
+const [loginusername] = defineField('loginusername');
 
-const onSubmit = handleSubmit((values) => {
+const onSubmit = handleSubmit( async(values, { resetForm }) => {
     console.log(JSON.stringify(values, null, 2));
+    loading.value = true
+    let result = ''
+    try {
+        if (values.id) {
+            result = await RestApi.post('api/v1/sg-5/create_useer_map/', values)
+        } else {
+            result = await RestApi.post('api/v1/sg-5/create_useer_map/', values)
+        }
+
+        if (result.status == 201) {
+            toast.success('User has been created!')
+            resetForm();
+            onToggle()
+            listReload('marker')
+        }
+
+    } catch (error) {
+        console.log('error', error)
+        myForm.value.setErrors({ form: mixin.cn(error, 'response.data', null) });
+    } finally {
+        loading.value = false
+    }
 });
 // watcher
 watchEffect(() => {
     // if (value) {
     //     value.userlatlong = value.loginusername
     // }
-    console.log('email - watchEffect', email)
+    console.log('email - watchEffect')
 })
 
 const onToggle = (formValue) => {
-    
-//   setValues({
-//     email: 'test@example.com',
-//   });
-  setFieldValue('email', 'test@example.com');
-    // console.log('formValue', formValue)
-    // schema.userlatlong = JSON.stringify(formValue)
-    // userLatLong.value = JSON.stringify(formValue)
-    // setFieldValue('userLatLong', JSON.stringify(formValue)); 
+
+    //   setValues({
+    //     email: 'test@example.com',
+    //   });
+    // setFieldValue('email', 'test@example.com');
+    setFieldValue('id', '');
+    setFieldValue('userid', 20);
+    setFieldValue('userlatlong', JSON.stringify(formValue));
     addUserModalRef.value.onToggle()
     // console.log('schema === modal open', schema)
 }
@@ -211,62 +221,46 @@ onMounted(async () => {
         <Form ref="myForm" @submit="onSubmit">
             <div class="flex-1 rounded-lg p-2 shadow-cyan-sm shadow-sm">
 
-                <!-- <div class="mb-2 pb-4">
-                    <label for="spilitter" class="input-label">Splitter</label>
-                    <Field name="spilitter" v-slot="{ field }">
-                        <v-select v-bind="field" :options="dropdownList.splitters" :reduce="item => item.value"
-                            id="spilitter" placeholder="Select Splitter" />
-                    </Field>
-                    <ErrorMessage class="error-text" name="spilitter" />
-                </div> -->
-                <!-- <div class="mb-2 pb-4">
-                    <label for="spilitter" class="input-label">Splitter</label>
-                        <v-select v-model="spilitter" :options="dropdownList.splitters" :reduce="item => item.value"
-                            id="spilitter" placeholder="Select Splitter" />
 
+                <div class="mb-2 pb-4">
+                    <label for="spilitter" class="input-label">Splitter</label>
+                    <v-select v-model="spilitter" :options="dropdownList.splitters" :reduce="item => item.value"
+                        id="spilitter" placeholder="Select Splitter" />
                     <p class="error-text">{{ errors.spilitter }}</p>
-                    <ErrorMessage class="error-text" name="spilitter" />
-                </div> -->
+                </div>
 
-                <!-- <div class="mb-2 pb-4">
+                <div class="mb-2 pb-4">
                     <label for="usertype" class="input-label">User Type</label>
                         <v-select v-model="usertype" :options="dropdownList.userTypes" :reduce="item => item.value"
                             id="usertype" placeholder="Select User Type" />
-                    <ErrorMessage class="error-text" name="usertype" />
+                    <p class="error-text">{{ errors.usertype }}</p>
                 </div>
 
                 <div class="mb-2 pb-4">
                     <label for="userlatlong" class="input-label">User Latlong</label>
                         <input type="text" v-model="userlatlong" id="userlatlong" class="input-control"
                             placeholder="Enter userlatlong" />
-                    <ErrorMessage class="error-text" name="userlatlong" />
+                    <p class="error-text">{{ errors.userlatlong }}</p>
                 </div>
 
                 <div class="mb-2 pb-4">
                     <label for="tjnumber" class="input-label">Tj Number</label>
-                        <input type="number" min="0" v-model="tjnumber" id="tjnumber" class="input-control"
+                        <input type="text" v-model.number="tjnumber" id="tjnumber" class="input-control"
                             placeholder="Enter tj number" />
-                    <ErrorMessage class="error-text" name="tjnumber" />
+                    <p class="error-text">{{ errors.tjnumber }}</p>
                 </div>
 
                 <div class="mb-2 pb-4">
                     <label for="lineby" class="input-label">Line By</label>
                     <input type="text" v-model="lineby" id="lineby" class="input-control" placeholder="Enter lineby" />
-                    <ErrorMessage class="error-text" name="lineby" />
+                    <p class="error-text">{{ errors.lineby }}</p>
                 </div>
 
                 <div class="mb-2 pb-4">
                     <label for="loginusername" class="input-label">Username</label>
                     <input type="text" v-model="loginusername" id="loginusername" class="input-control"
                         placeholder="Enter username" />
-                    <ErrorMessage class="error-text" name="loginusername" />
-                </div> -->
-
-                <div class="mb-2 pb-4">
-                    <label for="email" class="input-label">Email</label>
-                    <input type="email" v-model="email" id="email" class="input-control"
-                        placeholder="Enter Email"  />
-                    <p class="error-text">{{ errors.email }}</p>
+                    <p class="error-text">{{ errors.loginusername }}</p>
                 </div>
 
                 <div class="text-right">
